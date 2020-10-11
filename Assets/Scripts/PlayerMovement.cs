5c5177421eda49641059fa903b9f78bb56ff2ba8
savio15s <71825404+savio15s@users.noreply.github.com>
2020-10-12 01:25:54 +0530

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+   /* void Start() {
+        //Debug.Log("Hello WOrld!");
+        // rb.useGravity = false;
+        rb.AddForce(0, 200, 500);
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce* Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+        if(rb.position.y<-1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/Scripts/credits.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class credits : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
